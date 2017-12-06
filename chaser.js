577af405784b65ecf2de230014a833f2b604b705
rlaw2021 <33736959+rlaw2021@users.noreply.github.com>
2017-12-06 11:01:37 -0800

    restart game, sprites, push off, score

--- chaser.js
+++ chaser.js
@@ -1,6 +1,24 @@
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
 const progressBar = document.querySelector("progress");
+let score = document.getElementById("score")
+
+
+function startGame() {
+  if (progressBar.value === 0) {
+    progressBar.value = 100;
+    score.innerHTML = 0;
+    Object.assign(player, {x: canvas.width / 2, y: canvas.height / 2});
+    requestAnimationFrame(drawScene);
+  }
+}
+
+function increaseScore() {
+  let gameScore = score.innerHTML;
+  gameScore ++;
+  score.innerHTML = gameScore;
+  score = document.getElementById('score');
+}
 
 function distanceBetween(sprite1, sprite2) {
   return Math.hypot(sprite1.x - sprite2.x, sprite1.y - sprite2.y);
@@ -24,25 +42,35 @@ class Sprite {
 }
 
 class Player extends Sprite {
-  constructor(x, y, radius, color, stroke, speed) {
+  constructor(x, y, radius, speed) {
     super();
-    Object.assign(this, { x, y, radius, color, stroke, speed });
+    this.image = new Image();
+    this.image.src = "https://i.pinimg.com/originals/00/22/51/002251ab93aa8a09b5090fc4ad951f8c.png";
+    Object.assign(this, { x, y, radius, speed });
+  }
+  draw() {
+    ctx.drawImage(this.image, this.x - 30, this.y - 30, 60, 60);
   }
 }
 
-let player = new Player(250, 150, 25, "skyblue", "orange", 0.07);
+let player = new Player(250, 150, 25, 0.07);
 
 class Enemy extends Sprite {
-  constructor(x, y, radius, color, speed) {
+  constructor(x, y, radius, speed) {
     super();
-    Object.assign(this, { x, y, radius, color, speed });
+    this.image = new Image();
+    this.image.src = "https://vignette.wikia.nocookie.net/mm54321/images/9/9d/Vector-goomba.png/revision/latest?cb=20141123221433";
+    Object.assign(this, { x, y, radius, speed });
+  }
+  draw() {
+    ctx.drawImage(this.image, this.x - 30, this.y - 30, 60, 60);
   }
 }
 
 let enemies = [
-  new Enemy(80, 200, 20, "rgba(250, 0, 50 ,0.8)", 0.02),
-  new Enemy(200, 250, 17, "rgba(200, 100, 0, 0.7)", 0.01),
-  new Enemy(0, 0, 22, "rgba(50, 10, 70, 0.5)", 0.002)
+  new Enemy(80, 200, 20, 0.02),
+  new Enemy(200, 250, 17, 0.01),
+  new Enemy(0, 0, 22, 0.002)
 ];
 
 let mouse = { x: 0, y: 0 };
@@ -63,9 +91,21 @@ function follow(leader, follower, speed) {
   follower.y += (leader.y - follower.y) * speed;
 }
 
+function pushOff(c1, c2) {
+  let [dx, dy] = [c2.x - c1.x, c2.y - c1.y];
+  const L = Math.hypot(dx, dy);
+  let distToMove = c1.radius + c2.radius - L;
+  if (distToMove > 0) {
+    dx /= L; dy /= L;
+    c1.x -= dx * distToMove / 2; c1.y -= dy * distToMove / 2;
+    c2.x += dx * distToMove / 2; c2.y += dy * distToMove / 2; 
+  }
+}
+
 function updateScene() {
   follow(mouse, player, player.speed);
   enemies.forEach(enemy => follow(player, enemy, enemy.speed));
+    enemies.forEach((enemy, i) => pushOff(enemy, enemies[(i+1) % enemies.length]));
   enemies.forEach(enemy => {
     if (haveCollided(enemy, player)) {
       progressBar.value -= 0.5;
@@ -78,12 +118,16 @@ function drawScene() {
   player.draw();
   enemies.forEach(enemy => enemy.draw());
   updateScene();
+  increaseScore();
   if (progressBar.value <= 0) {
-    console.log('Game over');
+    ctx.font = '30px Arial';
+    ctx.fillText('The princess was in another castle, click to play again', 0, canvas.height/2);
   } else {
     requestAnimationFrame(drawScene);
   }
 }
 
+canvas.addEventListener('click', startGame);
 requestAnimationFrame(drawScene);
+
 
